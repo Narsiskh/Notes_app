2b5fc2df1c103f6bcf3e450f4f54550f75f13efc
Narsis <narsiskh.2013@gmail.com>
2023-11-01 20:20:01 +0330

    creating notes app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="overlay"></div>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button class="add">+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut maiores omnis quia suscipit. Unde, voluptas.</p>
+          <p class="date">05/03/2023</p>
+        </div>
+        <div class="card">
+          <p class="main-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut maiores omnis quia suscipit. Unde, voluptas.</p>
+          <p class="date">05/03/2023</p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style src="./assets/style.sass" scoped lang="sass"></style>
